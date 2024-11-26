74b3749c7bd3a6fb4ff3819c57b39535fb6dad26
Stefan Wienert <info@stefanwienert.de>
2024-11-26 16:35:57 +0000

    Feat: make validation optional to allow other VAT calculations

--- lib/secretariat/line_item.rb
+++ lib/secretariat/line_item.rb
@@ -84,8 +84,8 @@ module Secretariat
       TAX_CATEGORY_CODES[tax_category] || 'S'
     end
 
-    def to_xml(xml, line_item_index, version: 2)
-      if !valid?
+    def to_xml(xml, line_item_index, version: 2, validate: true)
+      if validate && !valid?
         pp errors
         raise ValidationError.new("LineItem #{line_item_index} is invalid", errors)
       end
